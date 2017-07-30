097c86db416b17587da7c5901f15a32aa757fc19
Rizky Nugraha <rizky@shofyan@gmail.com>
2017-07-30 15:15:14 +0700

    update meta

--- resources/views/frontpage/layouts/main.blade.php
+++ resources/views/frontpage/layouts/main.blade.php
@@ -5,10 +5,9 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	@yield('meta', '')
 
-	<title>@yield('title', config('app.name'))</title>
-	@yield('meta')
-	<!-- Fonts -->
+	<title>@yield('title', config('app.name'))</title><!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400,700,800" rel="stylesheet">
 	<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
 
